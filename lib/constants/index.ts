a62e939fc472896c45c279fb572033787457b191
Panucci <eyakubu442@gmail.com>
2025-11-19 11:25:52 +0100

    minor touch

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,7 +8,7 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: 'admin@example.com',
+  email: 'tv.switch08@example.com',
   password: '123456',
 };
 
